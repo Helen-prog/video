229bed6a9d34c98cb79b3fe67747f8663c6f5342
Helen <elena.kozovyakina@gmail.com>
2022-01-28 14:21:46 +0300

    project complete

---
+++ src/components/Preloader.js
@@ -0,0 +1,9 @@
+import './Preloader.css';
+
+function Preloader(){
+    return(
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Preloader;
