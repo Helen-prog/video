d8ea180e9550e6d2f642349720adae96db02bb7a
Helen <elena.kozovyakina@gmail.com>
2022-02-05 02:30:40 +0300

    pagination

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,46 +1,101 @@
-import React from 'react';
+import React from "react";
 import './Search.css';
 
 class Search extends React.Component {
     state = {
         search: "",
-        type: "all"
+        type: "all",
+        page: 1,
+
     }
 
-    handleKey = (event) => {
+
+    handlerKey = (event) => {
         if (event.key === "Enter") {
             this.props.searchMovies(this.state.search, this.state.type);
         }
     }
 
     handlerFilter = (event) => {
-        this.setState(() => ({type: event.target.dataset.type}), () => {
+        this.setState(() => ({ type: event.target.dataset.type }), () => {
             this.props.searchMovies(this.state.search, this.state.type);
         })
+
     }
 
-    render() {
+    prevPage = () => {
+        this.setState(
+            () => (this.state.page > 1 ? { page: this.state.page - 1 } : { page: 1 }),
+            () => (this.props.searchMovies(this.state.search, this.state.type, this.state.page))
+        )
+    }
+
+    nextPage = () => {
+        this.setState(
+            () => ({ page: this.state.page + 1 }),
+            () => (this.props.searchMovies(this.state.search, this.state.type, this.state.page))
+        )
+    }
+
+    setPage = (num) => {
+        this.setState(
+            () => ({ page: num }),
+            () => (this.props.searchMovies(this.state.search, this.state.type, this.state.page))
+        );
+    }
+
+
+    render() {        
+        let limit = 10;
+        let totalPages = Math.ceil(this.props.totalCount / limit);
+        // console.log("totalPages new", totalPages);
+        const lastIndex = totalPages <= 10 ? totalPages + 1 : this.state.page + limit;
+        // console.log("last index: ", lastIndex);
+        const firstIndex = totalPages <= 10 ? lastIndex - limit + lastIndex + 1 : lastIndex - limit // 1;
+        // console.log("first index: ", firstIndex);
+        let num = [];
+        for (let i = 0; i < totalPages + 1; i++) {
+            num.push(i);
+        }
         return (
-            <>
+            <>                
                 <div className="search">
                     <input
                         type="search"
-                        placeholder='search'
+                        placeholder="search"
                         value={this.state.search}
-                        onChange={(event) => this.setState({ search: event.target.value }
-                        )}
-                        onKeyDown={this.handleKey}
+                        onChange={(e) => this.setState({ search: e.target.value })} onKeyDown={this.handlerKey}
                     />
                     <button
-                        className='btn'
+                        className="btn"
                         onClick={() => this.props.searchMovies(this.state.search, this.state.type)}
                     >Search</button>
                 </div>
                 <div className="radio">
-                    <input type="radio" name="type" data-type="all" checked={this.state.type === "all"} onChange={this.handlerFilter} /> <span>All</span>
-                    <input type="radio" name="type" data-type="movie" checked={this.state.type === "movie"} onChange={this.handlerFilter} /> <span>Movies only</span>
-                    <input type="radio" name="type" data-type="series" checked={this.state.type === "series"} onChange={this.handlerFilter} /> <span>Series only</span>
-                    <input type="radio" name="type" data-type="game" checked={this.state.type === "game"} onChange={this.handlerFilter} /> <span>Games only</span>
+                    <input type="radio" name="type" data-type="all" checked={this.state.type === 'all'} onChange={this.handlerFilter} /> <span>All</span>
+                    <input type="radio" name="type" data-type="movie" checked={this.state.type === 'movie'} onChange={this.handlerFilter} /> <span>Movies only</span>
+                    <input type="radio" name="type" data-type="series" checked={this.state.type === 'series'} onChange={this.handlerFilter} /> <span>Series only</span>
+                    <input type="radio" name="type" data-type="game" checked={this.state.type === 'game'} onChange={this.handlerFilter} /> <span>Games only</span>
+                </div>
+                <div className="navigate">
+                    <button className="btn" onClick={this.prevPage}>Previous page</button>
+
+                    <div className="items">
+                        {num
+                            .slice(firstIndex, lastIndex)
+                            .map((el, index) => (                                
+                                <button
+                                    className="btn"
+                                    style={{ background: this.state.page !== el ? "gray" : "" }}
+                                    key={index}
+                                    onClick={() => (this.setPage(el))}
+                                >{el}</button>
+
+                            ))}
+                    </div>
+
+
+                    <button className="btn" onClick={this.nextPage}>Next page</button>
                 </div>
             </>
         )
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 import './Main.css';
-import MovieList from '../components/MovieList';
-import Preloader from '../components/Preloader';
-import Search from '../components/Search';
+import MovieList from "../components/MovieList";
+import Preloader from "../components/Preloader";
+import Search from "../components/Search";
 
-class Main extends React.Component{
+class Main extends React.Component {    
     state = {
         movies: [],
-        loading: true
+        loading: true,
+        count: 0
     }
 
-    componentDidMount(){
-        fetch("http://www.omdbapi.com/?apikey=4eb9d7fd&s=matrix")
+    componentDidMount() {
+        fetch("https://www.omdbapi.com/?apikey=4eb9d7fd&s=matrix")
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({ movies: data.Search, loading: false, count: data.totalResults }))
     }
 
-    searchMovies = (str, type="all") => {
+    searchMovies = (str, type = "all", page) => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=4eb9d7fd&s=${str}${type !== 'all' ? `&type=${type}`: ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=4eb9d7fd&s=${str}${type !== 'all' ? `&type=${type}` : ''}${`&page=${page}`}`)
             .then(response => response.json())
-            .then(data => this.setState({movies: data.Search, loading: false}))
+            .then(data => this.setState({ movies: data.Search, loading: false, count: data.totalResults }))        
     }
 
-    render(){
-        const {movies, loading} = this.state;
-        return(
+    render() {
+        const { movies, loading, count } = this.state;        
+        return (
             <div className="main">
                 <div className="wrap">
-                    <Search searchMovies={this.searchMovies} />
+                    <Search searchMovies={this.searchMovies} totalCount={count} />
                     {
-                        loading ? <Preloader /> : <MovieList movies={movies} />
-                    }                    
+                        loading ? <Preloader /> : <MovieList movies={movies} /> 
+                        
+                    }
+                   
                 </div>
             </div>
         )
